754917785b60741f956771431e759899572a72e3
= <=>
2019-05-29 22:11:36 -0400

    added express'

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 5000;
+
+// Static file declaration
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Production mode
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, 'client/build')));
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, 'client/build/index.html'));
+	})
+}
+
+// Build mode
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '/client/public/index.html'));
+})
 
 // Route setup
 app.get('/', (req, res) => {
