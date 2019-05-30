9c605997f58787be6d6d7d6250a8b537ea50d30a
Alejandro <lapinto.17@est.ucab.edu.ve>
2019-05-29 21:28:54 -0400

    Initial commit: added nodejs and react

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Route setup
+app.get('/', (req, res) => {
+	res.send('root route');
+})
+
+// Start server
+app.listen(port, (req, res) => {
+	console.log(`server listening on port: ${port}`)
+});
