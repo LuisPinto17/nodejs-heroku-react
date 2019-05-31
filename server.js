fd7f27929c92ffce7adf74d92078519658f293c7
= <=>
2019-05-31 03:19:20 -0400

    Removed quieres dependency

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 5000;
-const db = require('.client/src/queries');
 
 // Static file declaration
 app.use(express.static(path.join(__dirname, 'client/build')));
