40f8c2ac4e54d93eb1694c903c8ad31e87beeae9
= <=>
2019-05-30 23:21:10 -0400

    Fixed another mistake

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './stylesheets/App.css';
+import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from './containers/Login';
 
